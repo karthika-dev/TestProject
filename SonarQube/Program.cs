c25e336c879d109f67c9047f6ae37d7c9e500cf6
Karthika D <karthika.d@sreegcloudgmail.onmicrosoft.com>
2018-09-05 18:29:43 +0530

    prepared

--- SonarQube/Program.cs
+++ SonarQube/Program.cs
@@ -25,6 +25,7 @@ namespace SonarQube
             //tstingcsxcsaxsaxdscadxa//hgjjhjhkj
 
             int zx;
+            //sddwdw
             //wrwer
             //fsedfsefsd
             //hjghjhyui
